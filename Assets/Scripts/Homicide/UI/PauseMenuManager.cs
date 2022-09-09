85e73ea9c048596271c7bc0617bd41607645d9ce
Priyam-Swine <priyamj580@gmail.com>
2022-09-09 11:29:54 +1000

    Prefabed

---
+++ Assets/Scripts/Homicide/UI/PauseMenuManager.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenuManager : MonoBehaviour
+{
+    [SerializeField] string sceneToLoad;
+    [SerializeField] GameObject pauseCanvas;
+    public static bool gameIsPaused = false;
+  
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            gameIsPaused = !gameIsPaused;
+            Pause();
+        }
+    }
+
+    public void loadScene()
+    {
+        SceneManager.LoadScene(sceneToLoad);
+    }
+
+
+    public void Pause()
+    {
+        if(gameIsPaused)
+        {
+            Time.timeScale = 0f;
+            pauseCanvas.SetActive(true);
+        }
+        else
+        {
+            Resume();
+        }
+    }
+
+    void Resume()
+    {
+        Time.timeScale = 1f;
+        pauseCanvas.SetActive(false);
+    }
+
+    public void ResumeOnClick()
+    {
+        gameIsPaused = !gameIsPaused;
+        Resume();
+    }
+
+
+}
